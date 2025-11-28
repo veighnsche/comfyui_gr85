a5d384417723bfcc3db25ee2140c260d3a9ff873
Vince Liem <vincepaul.liem@gmail.com>
2025-11-28 04:48:51 +0100

    Simplify import paths to use relative imports
    Changed absolute imports from `custom_nodes.comfyui_gr85.nodes.*` to relative imports using `.nodes.*`. Consolidated multiple imports from `tag_injector` module into a single line.

--- __init__.py
+++ __init__.py
@@ -2,19 +2,17 @@ from typing_extensions import override
 
 from comfy_api.latest import ComfyExtension, io
 
-from custom_nodes.comfyui_gr85.nodes.resolution.image_dimension_resizer import ImageDimensionResizer
-from custom_nodes.comfyui_gr85.nodes.resolution.image_sizer import ImageSizer
-from custom_nodes.comfyui_gr85.nodes.resolution.image_sizer_all import ImageSizerAll
-from custom_nodes.comfyui_gr85.nodes.resolution.random_ratio import RandomRatio
-from custom_nodes.comfyui_gr85.nodes.prompt_selection.seed_based_output_selector import SeedBasedOutputSelector
-from custom_nodes.comfyui_gr85.nodes.prompt_wildcards.simple_wildcard_picker import SimpleWildcardPicker
-from custom_nodes.comfyui_gr85.nodes.prompt_tags.tag_injector import TagInjector
-from custom_nodes.comfyui_gr85.nodes.prompt_tags.tag_injector import TagInjectorSingle
-from custom_nodes.comfyui_gr85.nodes.prompt_tags.tag_injector import TagInjectorDuo
-from custom_nodes.comfyui_gr85.nodes.prompt_tags.tag_injector_large import TagInjectorLarge
-from custom_nodes.comfyui_gr85.nodes.random_seed.next_seed import NextSeed
-from custom_nodes.comfyui_gr85.nodes.random_numbers.random_float import RandomFloat
-from custom_nodes.comfyui_gr85.nodes.random_numbers.random_int import RandomInt
+from .nodes.resolution.image_dimension_resizer import ImageDimensionResizer
+from .nodes.resolution.image_sizer import ImageSizer
+from .nodes.resolution.image_sizer_all import ImageSizerAll
+from .nodes.resolution.random_ratio import RandomRatio
+from .nodes.prompt_selection.seed_based_output_selector import SeedBasedOutputSelector
+from .nodes.prompt_wildcards.simple_wildcard_picker import SimpleWildcardPicker
+from .nodes.prompt_tags.tag_injector import TagInjector, TagInjectorSingle, TagInjectorDuo
+from .nodes.prompt_tags.tag_injector_large import TagInjectorLarge
+from .nodes.random_seed.next_seed import NextSeed
+from .nodes.random_numbers.random_float import RandomFloat
+from .nodes.random_numbers.random_int import RandomInt
 
 NODE_CLASS_MAPPINGS = {}
 
