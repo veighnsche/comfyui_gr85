3d1b9064963194d4a9b64c161ff4cab203d0c6ac
Vince Liem <vincepaul.liem@gmail.com>
2025-11-28 03:31:13 +0100

    Reorganize node directory structure into functional categories
    Restructured node organization by moving files from generic directories into more specific functional categories. Moved resolution-related nodes from nodes/latent/ to nodes/resolution/, split prompt nodes into nodes/prompt_selection/, nodes/prompt_wildcards/, and nodes/prompt_tags/, moved utility nodes from nodes/utils/ to nodes/random_numbers/ and nodes/random_seed/. Updated all import paths in __init__.py to reflect the new directory

--- __init__.py
+++ __init__.py
@@ -2,19 +2,19 @@ from typing_extensions import override
 
 from comfy_api.latest import ComfyExtension, io
 
-from custom_nodes.comfyui_gr85.nodes.latent.image_dimension_resizer import ImageDimensionResizer
-from custom_nodes.comfyui_gr85.nodes.latent.image_sizer import ImageSizer
-from custom_nodes.comfyui_gr85.nodes.latent.image_sizer_all import ImageSizerAll
-from custom_nodes.comfyui_gr85.nodes.latent.random_ratio import RandomRatio
-from custom_nodes.comfyui_gr85.nodes.prompt.seed_based_output_selector import SeedBasedOutputSelector
-from custom_nodes.comfyui_gr85.nodes.prompt.simple_wildcard_picker import SimpleWildcardPicker
-from custom_nodes.comfyui_gr85.nodes.prompt.tag_injector import TagInjector
-from custom_nodes.comfyui_gr85.nodes.prompt.tag_injector import TagInjectorSingle
-from custom_nodes.comfyui_gr85.nodes.prompt.tag_injector import TagInjectorDuo
-from custom_nodes.comfyui_gr85.nodes.prompt.tag_injector_large import TagInjectorLarge
-from custom_nodes.comfyui_gr85.nodes.utils.next_seed import NextSeed
-from custom_nodes.comfyui_gr85.nodes.utils.random_float import RandomFloat
-from custom_nodes.comfyui_gr85.nodes.utils.random_int import RandomInt
+from custom_nodes.comfyui_gr85.nodes.resolution.image_dimension_resizer import ImageDimensionResizer
+from custom_nodes.comfyui_gr85.nodes.resolution.image_sizer import ImageSizer
+from custom_nodes.comfyui_gr85.nodes.resolution.image_sizer_all import ImageSizerAll
+from custom_nodes.comfyui_gr85.nodes.resolution.random_ratio import RandomRatio
+from custom_nodes.comfyui_gr85.nodes.prompt_selection.seed_based_output_selector import SeedBasedOutputSelector
+from custom_nodes.comfyui_gr85.nodes.prompt_wildcards.simple_wildcard_picker import SimpleWildcardPicker
+from custom_nodes.comfyui_gr85.nodes.prompt_tags.tag_injector import TagInjector
+from custom_nodes.comfyui_gr85.nodes.prompt_tags.tag_injector import TagInjectorSingle
+from custom_nodes.comfyui_gr85.nodes.prompt_tags.tag_injector import TagInjectorDuo
+from custom_nodes.comfyui_gr85.nodes.prompt_tags.tag_injector_large import TagInjectorLarge
+from custom_nodes.comfyui_gr85.nodes.random_seed.next_seed import NextSeed
+from custom_nodes.comfyui_gr85.nodes.random_numbers.random_float import RandomFloat
+from custom_nodes.comfyui_gr85.nodes.random_numbers.random_int import RandomInt
 
 NODE_CLASS_MAPPINGS = {
     "GR85_ImageDimensionResizer": ImageDimensionResizer,
